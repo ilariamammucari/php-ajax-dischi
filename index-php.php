c4a9d7be2f8ee12c8d958c8d478c453b2b2dccd0
ilariamammucari <>
2021-03-01 14:13:24 +0100

    init

---
+++ index-php.php
@@ -0,0 +1,29 @@
+<?php
+include 'db.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Esercizio dischi musicali</title>
+    <link rel="stylesheet" href="dist/style.css">
+</head>
+
+<body>
+    <main class="contenitore">
+        <div class="box-card">
+            <div class="card">
+                <img src="" alt="">
+                <h3 class="titolo-disco"></h3>
+                <h4 class="nome-autore"></h4>
+                <h6 class="anno"></h6>
+            </div>
+        </div>
+    </main>
+</body>
+
+</html>
---
+++ webpack.mix.js
@@ -0,0 +1,5 @@
+let mix = require('laravel-mix');
+
+mix.js('src/js/script.js', 'dist').sass('src/scss/style.scss', 'dist').options({
+    processCssUrls: false
+});
