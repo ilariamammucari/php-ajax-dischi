e96093e0b5b6ccf348f4ebf47e3b401603bac56a
ilariamammucari <>
2021-03-01 15:33:41 +0100

    preparo file per lavoro di domani

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+import Vue from 'vue';
+
+var app = new Vue({
+    el: '#root',
+    data: {
+
+    },
+    created(){
+        alert();
+    }
+});
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,5 @@
 let mix = require('laravel-mix');
 
-mix.js('src/js/script.js', 'dist').sass('src/scss/style.scss', 'dist').options({
+mix.js('src/js/script.js', 'dist').vue().sass('src/scss/style.scss', 'dist').options({
     processCssUrls: false
 });
