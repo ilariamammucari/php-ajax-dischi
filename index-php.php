c66594dc28190b2cbd4ee4c2a642670acdaaa2bb
ilariamammucari <>
2021-03-01 14:49:24 +0100

    creo database e popolo la pagina con i dischi

--- index-php.php
+++ index-php.php
@@ -10,18 +10,32 @@ include 'db.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Esercizio dischi musicali</title>
+    <link rel="icon" type="image/png" sizes="16x16" href="img/favicon.png">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="dist/style.css">
 </head>
 
 <body>
-    <main class="contenitore">
+    <header>
+        <h1>Dischi musicali</h1>
+    </header>
+    <main>
         <div class="box-card">
+            <?php 
+                foreach ( $dischi as $disco ) {
+                $img = $disco['img'];
+                $titolo = $disco['titolo'];
+                $autore = $disco['autore'];
+                $anno = $disco['anno'];
+            ?>
             <div class="card">
-                <img src="" alt="">
-                <h3 class="titolo-disco"></h3>
-                <h4 class="nome-autore"></h4>
-                <h6 class="anno"></h6>
+                <img src="<?= $img ?>" alt="poster">
+                <h3 class="titolo-disco"><?= $titolo ?></h3>
+                <h4 class="nome-autore"><?= $autore ?></h4>
+                <h6 class="anno"><?= $anno ?></h6>
             </div>
+            <?php } ?>
         </div>
     </main>
 </body>
