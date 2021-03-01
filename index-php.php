71e422a38300719879d32cdb62384f128a3947ed
ilariamammucari <>
2021-03-01 15:50:37 +0100

    index.php pulito

--- index-php.php
+++ index-php.php
@@ -17,29 +17,27 @@ include 'db.php';
 </head>
 
 <body>
-    <div id="root">
-        <header>
-            <h1>Dischi musicali</h1>
-        </header>
-        <main>
-            <div class="box-card">
-                <?php 
-                    foreach ( $dischi as $disco ) {
-                    $img = $disco['img'];
-                    $titolo = $disco['titolo'];
-                    $autore = $disco['autore'];
-                    $anno = $disco['anno'];
-                ?>
-                <div class="card">
-                    <img src="<?= $img ?>" alt="poster">
-                    <h3 class="titolo-disco"><?= $titolo ?></h3>
-                    <h4 class="nome-autore"><?= $autore ?></h4>
-                    <h6 class="anno"><?= $anno ?></h6>
-                </div>
-                <?php } ?>
+    <header>
+        <h1>Dischi musicali</h1>
+    </header>
+    <main>
+        <div class="box-card">
+            <?php 
+                foreach ( $dischi as $disco ) {
+                $img = $disco['img'];
+                $titolo = $disco['titolo'];
+                $autore = $disco['autore'];
+                $anno = $disco['anno'];
+            ?>
+            <div class="card">
+                <img src="<?= $img ?>" alt="poster">
+                <h3 class="titolo-disco"><?= $titolo ?></h3>
+                <h4 class="nome-autore"><?= $autore ?></h4>
+                <h6 class="anno"><?= $anno ?></h6>
             </div>
-        </main>
-    </div>
+            <?php } ?>
+        </div>
+    </main>
 </body>
 
 </html>
